c25a00e06315a8655430f99b6e681781280915dc
Yohai Rosen <yohai.roz@gmail.com>
2018-04-17 21:26:00 +0300

    updated loader

---
+++ src/main.js
@@ -0,0 +1,15 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+
+var _Schedule = require('./Schedule');
+
+var _Schedule2 = _interopRequireDefault(_Schedule);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+// exports.default = _Schedule2.default;
+// module.exports = exports['default'];
+module.exports.Schedule = _Schedule2.Schedule
